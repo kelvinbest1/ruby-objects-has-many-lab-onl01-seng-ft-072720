4e1f575ffcec4fb561e9a1a90252fe37aeca88c0
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-12 17:01:53 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
